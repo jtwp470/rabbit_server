8ae35b3e4c4be87724b0172189af5420c87da4f0
Ryosuke Sato <rskjtwp@gmail.com>
2015-12-15 22:23:49 +0900

    Add new debug tool

--- rabbit_server/__init__.py
+++ rabbit_server/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 
 
 app = Flask(__name__)
-app.debug = True
-app.config.from_object('rabbit_server.config')
 
+# debug tool is deactive on production
+app.debug = True
+app.config['SECRET_KEY'] = "SECRETSECRETSECRET"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+toolbar = DebugToolbarExtension(app)
+
+
+# confs
+app.config.from_object('rabbit_server.config')
 app.jinja_env.globals['apptitle'] = "Rabbit CTF Score Server"
+
+# DB
 db = SQLAlchemy(app)
+
 
 import rabbit_server.views
